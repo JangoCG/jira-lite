587370419d6d6276820279c5b6f164d97da83386
CENGIZ GUERTUSGIL <cengiz.guertusgil@exxeta.com>
2021-09-06 23:37:43 +0200

    add category

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -7,6 +7,7 @@ export const Sidebar: React.FC<NavigationProps> = ({}) => {
       <div className="projectInfo">
         <div className="projectText">
           <div className="projectName">Jango V1.0</div>
+          <div className="category"></div>
         </div>
       </div>
       <div className="divider" />
